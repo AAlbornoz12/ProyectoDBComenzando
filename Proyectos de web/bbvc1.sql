35e59af7fce7e3ee1f1877d2174bbbce8d0d8303
albornoz <albornoz_13-25@hotmail.com>
2018-04-19 11:11:41 +0200

    nueva actualizacion del proyecto

---
+++ Proyectos de web/bbvc1.sql
@@ -0,0 +1,11 @@
+/*
+MYSQL DATA TRANSFER
+SOURCE HOST: localhost
+SOURCE DATABASE: DragonDiamante
+DATE: 09/02/2018  11:10 am
+*/
+DROP DATABASE IF EXISTS 'DragonDiamante';
+CREATE DATABASE DragonDiamante;
+use DragonDiamante
+
+CREATE 
---
+++ Proyectos html/dbdiamante.sql
@@ -0,0 +1,27 @@
+/* Navicat MySQL Transferencia de datos
+
+Servidor de origen: local
+Version de origen: 50555
+Fuente host: localhost: 3306
+Base de Datos Fuente: DB_Diamante
+
+Tipo de servidor de destino: MYSQL
+Version de servidor de destino: 50555
+codificacion de archivo: 65002
+
+DATE: 2018-16-03 12:52
+*/
+DROP DATABASE IF EXISTS `DBDiamante`;
+CREATE DATABASE DBDIAMANTE;
+USE DBDIAMANTE;
+SET FOREIGN_KEY_CHECKS=0;
+--------------------------------
+--TABLA DE ESTRUCTURA DE FRIENDS
+--------------------------------
+DROP TABLE IF EXISTS FRIENDS;
+CREATE TABLE'FRIENDS'(
+Nombre:
+Apellido:
+
+
+)
